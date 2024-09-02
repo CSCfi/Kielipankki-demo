b7f93703a113ad2ccaf7ab3687aab63505c1c546
Sam Hardwick <sam.hardwick@csc.fi>
2024-09-02 13:04:38 +0300

    Path fix

--- files/predict/s24_majority_vote.py
+++ files/predict/s24_majority_vote.py
@@ -7,23 +7,25 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import embutils
 
-rootpath = "/var/www/kielipankki-tools/predict/"
+rootpath = os.path.dirname(__file__) + "/"
+
 
 class Args:
     def __init__(self):
         self.lr = 0.001
         self.epochs = 256
-        self.batch_size=64
-        self.dropout=0.5
-        self.max_norm=3.0
-        self.embed_dim=128
+        self.batch_size = 64
+        self.dropout = 0.5
+        self.max_norm = 3.0
+        self.embed_dim = 128
         self.class_num = 3
         self.cuda = False
-        self.kernel_sizes = [2,3,4,5]
-        self.snapshot = rootpath + 's24_majority_vote_model.pt'
+        self.kernel_sizes = [2, 3, 4, 5]
+        self.snapshot = rootpath + "s24_majority_vote_model.pt"
         self.kernel_num = 100
         self.device = -1
-        self.static=True
+        self.static = True
+
 
 # model
 class CNN_Text(nn.Module):
@@ -34,7 +36,7 @@ class CNN_Text(nn.Module):
         self.embs.load_from_file(rootpath + "s24_majority_vote_vecs.bin")
 
         self.steps = 0
-        
+
         D = args.embed_dim
         C = args.class_num
         Ci = 1
@@ -42,13 +44,13 @@ class CNN_Text(nn.Module):
         Ks = args.kernel_sizes
 
         self.convs1 = nn.ModuleList([nn.Conv2d(Ci, Co, (K, D)) for K in Ks])
-        '''
+        """
         self.conv13 = nn.Conv2d(Ci, Co, (3, D))
         self.conv14 = nn.Conv2d(Ci, Co, (4, D))
         self.conv15 = nn.Conv2d(Ci, Co, (5, D))
-        '''
+        """
         self.dropout = nn.Dropout(args.dropout)
-        self.fc1 = nn.Linear(len(Ks)*Co, C)
+        self.fc1 = nn.Linear(len(Ks) * Co, C)
 
     def embed(self, texts):
         veclists = []
@@ -57,15 +59,17 @@ class CNN_Text(nn.Module):
             for token in text:
                 emb = self.embs.get_embedding(token)
                 veclists[-1].append(emb[1])
+
         def pad(veclist, to, padding):
             while len(veclist) < to:
                 veclist.append(padding)
+
         maxlen = max(max(map(len, texts)), max(self.args.kernel_sizes))
         padding = [0.0] * self.args.embed_dim
         for veclist in veclists:
             pad(veclist, maxlen, padding)
         return torch.Tensor(veclists)
-        
+
     def conv_and_pool(self, x, conv):
         x = F.relu(conv(x)).squeeze(3)  # (N, Co, W)
         x = F.max_pool1d(x, x.size(2)).squeeze(2)
@@ -74,35 +78,38 @@ class CNN_Text(nn.Module):
     def forward(self, x):
         x = list(x)
         x = self.embed(x)  # (N, W, D)
-        
+
         if self.args.static:
             x = Variable(x)
 
         x = x.unsqueeze(1)  # (N, Ci, W, D)
 
-        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1]  # [(N, Co, W), ...]*len(Ks)
+        x = [
+            F.relu(conv(x)).squeeze(3) for conv in self.convs1
+        ]  # [(N, Co, W), ...]*len(Ks)
 
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N, Co), ...]*len(Ks)
 
         x = torch.cat(x, 1)
 
-        '''
+        """
         x1 = self.conv_and_pool(x,self.conv13) #(N,Co)
         x2 = self.conv_and_pool(x,self.conv14) #(N,Co)
         x3 = self.conv_and_pool(x,self.conv15) #(N,Co)
         x = torch.cat((x1, x2, x3), 1) # (N,len(Ks)*Co)
-        '''
+        """
         x = self.dropout(x)  # (N, len(Ks)*Co)
-        #logit = torch.squeeze(self.fc1(x))  # (N, 1)
+        # logit = torch.squeeze(self.fc1(x))  # (N, 1)
         logit = self.fc1(x)  # (N, C)
         return logit
-        
+
 
 static_args = Args()
 cnn = CNN_Text(static_args)
 if static_args.snapshot is not None:
     # print('\nLoading model from {}...'.format(args.snapshot))
     cnn.load_state_dict(torch.load(static_args.snapshot))
+
 
 def predict(texts):
     preds = ["negative", "neutral", "positive"]
@@ -111,11 +118,46 @@ def predict(texts):
     for prediction in cnn(texts):
         results.append(preds[torch.argmax(prediction)])
     return results
-    
-if __name__ == '__main__':
-    print(str(predict([['Tosi', 'hyvä', 'juttu', '!'], ['Tosi', 'huono', 'juttu', '!'], ['Elokuva', 'oli', 'miellyttävä', '.'], ['Elokuva', 'oli', 'puuduttava', '.'], ['Elokuva', 'oli', 'ihan', 'ok.'], ['Tosi', 'hyvä', 'juttu', '!', 'Tosi', 'huono', 'juttu', '!', 'Elokuva', 'oli', 'miellyttävä', '.', 'Elokuva', 'oli', 'puuduttava', '.', 'Elokuva', 'oli', 'ihan', 'ok.']])))
+
+
+if __name__ == "__main__":
+    print(
+        str(
+            predict(
+                [
+                    ["Tosi", "hyvä", "juttu", "!"],
+                    ["Tosi", "huono", "juttu", "!"],
+                    ["Elokuva", "oli", "miellyttävä", "."],
+                    ["Elokuva", "oli", "puuduttava", "."],
+                    ["Elokuva", "oli", "ihan", "ok."],
+                    [
+                        "Tosi",
+                        "hyvä",
+                        "juttu",
+                        "!",
+                        "Tosi",
+                        "huono",
+                        "juttu",
+                        "!",
+                        "Elokuva",
+                        "oli",
+                        "miellyttävä",
+                        ".",
+                        "Elokuva",
+                        "oli",
+                        "puuduttava",
+                        ".",
+                        "Elokuva",
+                        "oli",
+                        "ihan",
+                        "ok.",
+                    ],
+                ]
+            )
+        )
+    )
 #     def handle_inputs(inputs):
-#         preds = 
+#         preds =
 #         for i, prediction in enumerate(predict(cnn, map(lambda x: x.split(), inputs))):
 #             print(, end = (f"\t{inputs[i]}\n" if args.print_input else "\n"))
 
