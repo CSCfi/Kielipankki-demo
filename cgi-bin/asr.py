ecba69d48a7df358476644de5e135c0f4c115d4f
Sam Hardwick <sam.hardwick@csc.fi>
2024-10-02 15:37:48 +0300

    Blackify

--- cgi-bin/asr.py
+++ cgi-bin/asr.py
@@ -8,24 +8,26 @@ import os
 import requests
 import json
 import cgitb
+
 cgitb.enable()
-#from tempfile import TemporaryFile
-#import urllib
+# from tempfile import TemporaryFile
+# import urllib
 import hashlib
 
 from kpdemos import *
+
 
 def print_content():
     jobid = None
     done = False
     query = os.environ.get("QUERY_STRING")
-    target_params = ''
+    target_params = ""
     if query is not None:
-        target_params = '?' + query
+        target_params = "?" + query
         query_dict = {}
-        if '=' in query:
-            query_dict = dict(map(lambda x: x.split('='), query.split('&')))
-        if 'job' in query_dict:
+        if "=" in query:
+            query_dict = dict(map(lambda x: x.split("="), query.split("&")))
+        if "job" in query_dict:
             jobid = query_dict["job"]
     # # urllib.parse(url).query
 
@@ -35,12 +37,16 @@ def print_content():
     result = ""
     inputval = ""
     columns = ["word", "start", "end"]
-    if ("file" in form and form["file"].filename != ""):
+    if "file" in form and form["file"].filename != "":
         filename = form["file"].filename
         inputfile = form["file"]
-        files = {'file': (filename, inputfile.file),
-                          'Content-Disposition': 'form-data; name="file"; filename="' + filename + '"',
-                          'Content-Type': 'multipart/form-data'}
+        files = {
+            "file": (filename, inputfile.file),
+            "Content-Disposition": 'form-data; name="file"; filename="'
+            + filename
+            + '"',
+            "Content-Type": "multipart/form-data",
+        }
         # files = {'file': (filename, inputfile.file, 'application/octet-stream'),
         #          'Content-Disposition': 'form-data; name="file"; filename="' + filename + '"',
         #          'Content-Type': 'application/octet-stream'}
@@ -49,12 +55,12 @@ def print_content():
         submit_url = "http://kielipankki.2.rahtiapp.fi/audio/asr/fi/submit_file"
         # submit_url = 'http://kielipankki.2.rahtiapp.fi/audio/asr/fi/segmented'
         try:
-            response = requests.post(submit_url, files = files)
+            response = requests.post(submit_url, files=files)
         except exception as e:
             result += str(e)
         job = json.loads(response.text)
         jobid = job["jobid"]
-        target_params = '?job=' + jobid
+        target_params = "?job=" + jobid
         time.sleep(1)
     if jobid is not None:
         query_url = "http://kielipankki.2.rahtiapp.fi/audio/asr/fi/query_job/tekstiks"
@@ -62,39 +68,51 @@ def print_content():
         j = json.loads(response.text)
         if "done" not in j:
             if "status" in j and j["status"] == "pending":
-                result = '<p>In queue..</p>'
+                result = "<p>In queue..</p>"
             else:
-                result = '<p>Got malformed response from server:</p>\n' + wrap_in_tags(json.dumps(j, indent=2), 'p')
+                result = "<p>Got malformed response from server:</p>\n" + wrap_in_tags(
+                    json.dumps(j, indent=2), "p"
+                )
         else:
             if j["done"] == False:
-                result = '<p>Partial result, wait for more..</p>\n'
+                result = "<p>Partial result, wait for more..</p>\n"
             else:
                 done = True
                 all_words = []
                 session_key = jobid
                 time_spent = j["processing_finished"] - j["processing_started"]
                 result = '<h3>Full transcript:</h3><div class="text-wrap" style="width: 80rem;"><hr><p class="text">{FULL_TRANSCRIPT}</p><hr></div><p><small>Decoding completed in {TIME_SPENT:.2f} seconds</small></p>'
-                result += '''
+                result += """
                 <div class="row">
                 <div class="col-md-auto py-4">
                 <a class="btn btn-info" href="https://kielipankki.fi/tools/demo/kielipankki-tools/tmp/{filename}.tsv" download="asr_result.tsv" role="button">Download TSV</a></div></div>
-            '''.format(html_root = hostname, filename = session_key)
+            """.format(
+                    html_root=hostname, filename=session_key
+                )
 
             json_result = j["result"]
             transcripts = []
             for section in json_result["sections"]:
                 start_time = float(section["start"])
-                result += wrap_in_tags(wrap_in_tags(section["transcript"], 'p'), 'b')
+                result += wrap_in_tags(wrap_in_tags(section["transcript"], "p"), "b")
                 words = []
                 for word in section["words"]:
-                    words.append([word["word"], '{:.2f}'.format(start_time + float(word["start"])), '{:.2f}'.format(start_time + float(word["end"]))])
+                    words.append(
+                        [
+                            word["word"],
+                            "{:.2f}".format(start_time + float(word["start"])),
+                            "{:.2f}".format(start_time + float(word["end"])),
+                        ]
+                    )
                     if done:
                         all_words.append(words[-1])
-                result += make_table(words, header = columns) + "\n"
+                result += make_table(words, header=columns) + "\n"
                 transcripts.append(section["transcript"])
             if done:
-                result = result.format(FULL_TRANSCRIPT = '\n<br>'.join(transcripts), TIME_SPENT=time_spent)
-                
+                result = result.format(
+                    FULL_TRANSCRIPT="\n<br>".join(transcripts), TIME_SPENT=time_spent
+                )
+
             # if ("status" in j and j["status"] == "pending") or ("done" in j and j["done"] == False):
             #     continue
             # else:
@@ -106,7 +124,7 @@ def print_content():
             write_tsv(make_tsv([columns] + all_words), session_key)
 
         # write_excel([column_names] + out_rows, session_key, "Output from fintag")
-            
+
         # result += "<p>Result:</p>\n"
         # <a class="btn btn-info" href="{html_root}/kielipankki-tools/tmp/{filename}.xlsx" download="ner_tagged.xlsx" role="button">Download Excel spreadsheet</a>
         # </div>
@@ -115,7 +133,7 @@ def print_content():
         # result += make_table(out_rows, header = column_names) + "\n"
 
     body = wrap_in_tags("asr demo", "h2")
-    body += '''
+    body += """
 <h6>Recognise Finnish speech with Kaldi and Aalto-asr</h6>
 <a href="#help" data-toggle="collapse">Show help</a>
 <div class="collapse" id="help">
@@ -155,12 +173,27 @@ def print_content():
   </div>
 </div>
 <p><small>Page generated in {TIME_SPENT:.2f} seconds</small></p>
-'''.format(scriptname = os.path.basename(sys.argv[0]), content = result, TIME_SPENT = time.time() - time_start)
+""".format(
+        scriptname=os.path.basename(sys.argv[0]),
+        content=result,
+        TIME_SPENT=time.time() - time_start,
+    )
     if jobid is None or done == True:
-        head = make_head(title = 'asr demo')
+        head = make_head(title="asr demo")
     else:
-        head = make_head(title = 'asr demo', extra_meta = '<meta http-equiv="refresh" content="2; url=asr.py{refresh_target}">'.format(refresh_target = target_params))
-    sys.stdout.buffer.write(wrap_html(head, wrap_in_tags(body, 'div', attribs='class="container pt-1"', oneline = False)).encode("utf-8"))
+        head = make_head(
+            title="asr demo",
+            extra_meta='<meta http-equiv="refresh" content="2; url=asr.py{refresh_target}">'.format(
+                refresh_target=target_params
+            ),
+        )
+    sys.stdout.buffer.write(
+        wrap_html(
+            head,
+            wrap_in_tags(body, "div", attribs='class="container pt-1"', oneline=False),
+        ).encode("utf-8")
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print_content()
--- cgi-bin/forced-align.py
+++ cgi-bin/forced-align.py
@@ -8,24 +8,26 @@ import os
 import requests
 import json
 import cgitb
+
 cgitb.enable()
-#from tempfile import TemporaryFile
-#import urllib
+# from tempfile import TemporaryFile
+# import urllib
 import hashlib
 
 from kpdemos import *
+
 
 def print_content():
     jobid = None
     done = False
     query = os.environ.get("QUERY_STRING")
-    target_params = ''
+    target_params = ""
     if query is not None:
-        target_params = '?' + query
+        target_params = "?" + query
         query_dict = {}
-        if '=' in query:
-            query_dict = dict(map(lambda x: x.split('='), query.split('&')))
-        if 'job' in query_dict:
+        if "=" in query:
+            query_dict = dict(map(lambda x: x.split("="), query.split("&")))
+        if "job" in query_dict:
             jobid = query_dict["job"]
     # # urllib.parse(url).query
 
@@ -35,16 +37,21 @@ def print_content():
     result = ""
     inputval = ""
     columns = ["start", "end", "word"]
-    if ("audio-file" in form and "transcript-file" in form):
+    if "audio-file" in form and "transcript-file" in form:
         # audio_filename = form["audio-file"].filename
         # transcript_filename = form["transcript-file"].filename
         # audio_fobj = form["audio-file"].filename
         # transcript_filename = form["transcript-file"].filename
 
-        files = {'audio': (form["audio-file"].filename, form["audio-file"].file),
-                 'transcript': (form["transcript-file"].filename, form["transcript-file"].file),
-                 'Content-Disposition': 'form-data',
-                 'Content-Type': 'multipart/form-data'}
+        files = {
+            "audio": (form["audio-file"].filename, form["audio-file"].file),
+            "transcript": (
+                form["transcript-file"].filename,
+                form["transcript-file"].file,
+            ),
+            "Content-Disposition": "form-data",
+            "Content-Type": "multipart/form-data",
+        }
         # files = {'file': (filename, inputfile.file, 'application/octet-stream'),
         #          'Content-Disposition': 'form-data; name="file"; filename="' + filename + '"',
         #          'Content-Type': 'application/octet-stream'}
@@ -52,13 +59,13 @@ def print_content():
         # audiobytes = inputfile.file.read()
         submit_url = "http://kielipankki.2.rahtiapp.fi/audio/align/fi/submit_file"
         try:
-            response = requests.post(submit_url, files = files)
+            response = requests.post(submit_url, files=files)
         except exception as e:
             result += str(e)
         log(response.text)
         job = json.loads(response.text)
         jobid = job["jobid"]
-        target_params = '?job=' + jobid
+        target_params = "?job=" + jobid
         time.sleep(1)
     if jobid is not None:
         query_url = "http://kielipankki.2.rahtiapp.fi/audio/align/fi/query_job"
@@ -66,22 +73,22 @@ def print_content():
         log(response.text)
         j = json.loads(response.text)
         if "status" in j and j["status"] == "pending":
-            result = '<p>In queue..</p>'
+            result = "<p>In queue..</p>"
         elif "status" in j and j["status"] == "done":
             done = True
             session_key = jobid
             ctm = j["results"]["ctm"]
             eaf = j["results"]["eaf"]
             TextGrid = j["results"]["TextGrid"]
-            ctm_words = ctm.split('\n')
+            ctm_words = ctm.split("\n")
             n_words = len(ctm_words)
             preview_words = [word.split() for word in ctm_words[:10]]
             if n_words > 10:
-                preview_words.append(['...','...','...'])
-            #time_spent = j["processing_finished"] - j["processing_started"]
-            #result = '<h3>Full transcript:</h3><div class="text-wrap" style="width: 80rem;"><hr><p class="text">{FULL_TRANSCRIPT}</p><hr></div><p><small>Decoding completed in {TIME_SPENT:.2f} seconds</small></p>'
+                preview_words.append(["...", "...", "..."])
+            # time_spent = j["processing_finished"] - j["processing_started"]
+            # result = '<h3>Full transcript:</h3><div class="text-wrap" style="width: 80rem;"><hr><p class="text">{FULL_TRANSCRIPT}</p><hr></div><p><small>Decoding completed in {TIME_SPENT:.2f} seconds</small></p>'
 
-            result += '''
+            result += """
             <div class="row">
             <div class="col-md-auto py-4">
             <a class="btn btn-info" href="https://kielipankki.fi/tools/demo/kielipankki-tools/tmp/{filename}.ctm" download="align_result.ctm" role="button">Download ctm</a></div>
@@ -89,10 +96,15 @@ def print_content():
             <a class="btn btn-info" href="https://kielipankki.fi/tools/demo/kielipankki-tools/tmp/{filename}.eaf" download="align_result.eaf" role="button">Download eaf</a></div>
             <div class="col-md-auto py-4">
             <a class="btn btn-info" href="https://kielipankki.fi/tools/demo/kielipankki-tools/tmp/{filename}.TextGrid" download="align_result.TextGrid" role="button">Download TextGrid</a></div></div>
-            '''.format(html_root = hostname, filename = session_key)
+            """.format(
+                html_root=hostname, filename=session_key
+            )
 
-
-            result += wrap_in_tags('Preview', 'h3') + make_table(preview_words, header = columns) + "\n"
+            result += (
+                wrap_in_tags("Preview", "h3")
+                + make_table(preview_words, header=columns)
+                + "\n"
+            )
             # for section in json_result["sections"]:
             #     start_time = float(section["start"])
             #     result += wrap_in_tags(wrap_in_tags(section["transcript"], 'p'), 'b')
@@ -105,7 +117,7 @@ def print_content():
             #     transcripts.append(section["transcript"])
             # if done:
             #     result = result.format(FULL_TRANSCRIPT = '\n<br>'.join(transcripts), TIME_SPENT=time_spent)
-                
+
             # if ("status" in j and j["status"] == "pending") or ("done" in j and j["done"] == False):
             #     continue
             # else:
@@ -113,7 +125,9 @@ def print_content():
             #     break
             result_rows = []
         else:
-            result = '<p>Got malformed response from server:</p>\n' + wrap_in_tags(json.dumps(j, indent=2), 'p')
+            result = "<p>Got malformed response from server:</p>\n" + wrap_in_tags(
+                json.dumps(j, indent=2), "p"
+            )
 
         if done:
             write_file(ctm, "ctm", session_key)
@@ -121,7 +135,7 @@ def print_content():
             write_file(TextGrid, "TextGrid", session_key)
 
         # write_excel([column_names] + out_rows, session_key, "Output from fintag")
-            
+
         # result += "<p>Result:</p>\n"
         # <a class="btn btn-info" href="{html_root}/kielipankki-tools/tmp/{filename}.xlsx" download="ner_tagged.xlsx" role="button">Download Excel spreadsheet</a>
         # </div>
@@ -130,7 +144,7 @@ def print_content():
         # result += make_table(out_rows, header = column_names) + "\n"
 
     body = wrap_in_tags("forced-align demo", "h2")
-    body += '''
+    body += """
 <h6>Align Finnish audio with transcript</h6>
 <a href="#help" data-toggle="collapse">Show help</a>
 <div class="collapse" id="help">
@@ -176,14 +190,29 @@ def print_content():
   </div>
 </div>
 <p><small>Page generated in {TIME_SPENT:.2f} seconds</small></p>
-'''.format(scriptname = os.path.basename(sys.argv[0]), content = result, TIME_SPENT = time.time() - time_start)
+""".format(
+        scriptname=os.path.basename(sys.argv[0]),
+        content=result,
+        TIME_SPENT=time.time() - time_start,
+    )
     if jobid is None or done == True:
-        head = make_head(title = 'align demo')
+        head = make_head(title="align demo")
     else:
-        head = make_head(title = 'align demo', extra_meta = '<meta http-equiv="refresh" content="2; url=forced-align.py{refresh_target}">'.format(refresh_target = target_params))
-    sys.stdout.buffer.write(wrap_html(head, wrap_in_tags(body, 'div', attribs='class="container pt-1"', oneline = False)).encode("utf-8"))
+        head = make_head(
+            title="align demo",
+            extra_meta='<meta http-equiv="refresh" content="2; url=forced-align.py{refresh_target}">'.format(
+                refresh_target=target_params
+            ),
+        )
+    sys.stdout.buffer.write(
+        wrap_html(
+            head,
+            wrap_in_tags(body, "div", attribs='class="container pt-1"', oneline=False),
+        ).encode("utf-8")
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         print_content()
     except Exception as ex:
